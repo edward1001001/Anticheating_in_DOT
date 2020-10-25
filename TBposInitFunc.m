d7c7849296706d77841fa498e57aa16008027523
edward1001001 <48808972+edward1001001@users.noreply.github.com>
2020-10-24 23:56:00 -0400

    Delete TBposInitFunc.m

--- TBposInitFunc.m
+++
@@ -1,8 +0,0 @@
-function TBpos = TBposInitFunc(PSTA, TBposFull)
-% position term table
-N = length(PSTA);
-TBpos = zeros(N,N); % TBpos(i,j) means z copied by Pos_j from Pos_i
-for i = 1:N
-    TBpos(i,:) = TBposFull(PSTA(i),PSTA);
-end
-end
